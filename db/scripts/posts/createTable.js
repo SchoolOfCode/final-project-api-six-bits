ac4ee652df8f391499c08e849dad0dcc9dcb7a90
Sebastian Butoi <butoi.sebastian@yahoo.com>
2022-02-21 15:04:50 +0000

    Routes concatenated

--- db/scripts/posts/createTable.js
+++ db/scripts/posts/createTable.js
@@ -1,9 +1,8 @@
-import db from "../../connection.js";
+import query from "../../connection.js";
 
-const response = await db.query(
+const response = await query(
   `CREATE TABLE IF NOT EXISTS posts (id SERIAL PRIMARY KEY, title TEXT, full_name TEXT, description TEXT, location TEXT, phone_number TEXT, email TEXT, free BOOLEAN NOT NULL, price NUMERIC, date TEXT);`
 );
 
 console.log(response);
 
-db.end();
--- routes/posts.js
+++ routes/posts.js
@@ -3,10 +3,6 @@ import { insertPost, readPosts } from "../models/posts.js";
 
 const router = express.Router();
 
-/* GET posts listing. */
-router.get("/", readPosts);
-
-/* POST post listing. */
-router.post("/", insertPost);
+router.route("/").get(readPosts).post(insertPost);
 
 export default router;
