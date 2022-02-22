9c6357738105c1a6d3a987efceb7da13299d75a7
theusufructuary <pentamingforall@gmail.com>
2022-02-22 16:23:33 +0000

    fixed bug with user_id being referenced in posts table

--- db/scripts/posts/createTable.js
+++ db/scripts/posts/createTable.js
@@ -1,7 +1,7 @@
 import query from "../../connection.js";
 
 const response = await query(
-  `CREATE TABLE IF NOT EXISTS posts (post_id SERIAL PRIMARY KEY, user_id INTEGER, title TEXT, description TEXT, location TEXT, free BOOLEAN, price NUMERIC, date TEXT, FOREIGN KEY user_id REFERENCES users(user_id));`
+  `CREATE TABLE IF NOT EXISTS posts (post_id SERIAL PRIMARY KEY, user_id INTEGER, title TEXT, description TEXT, location TEXT, free BOOLEAN, price NUMERIC, date TEXT, FOREIGN KEY (user_id) REFERENCES users(user_id));`
 );
 
 console.log(response);
