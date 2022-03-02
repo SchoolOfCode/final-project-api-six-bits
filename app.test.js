6f258655a1bfc6de578379ac438e46901df8460a
Sebastian Butoi <butoi.sebastian@yahoo.com>
2022-03-02 14:30:35 +0000

    zy, first test working

--- app.test.js
+++ app.test.js
@@ -1,14 +1,35 @@
 import request from "supertest";
-
 import app from "./app.js";
 
-describe("GET /users", function () {
+describe("GET /api/posts", function () {
   test("gives us back 200, with a message", async function () {
     const expectedBody = {
-      message: "I wish we had some information to give you ☹️"
+      status: "success",
+      message: "Read all posts",
     };
-    const actual = await request(app).get("/users");
-    expect(actual.body).toStrictEqual(expectedBody);
+    const actual = await request(app).get("/api/posts");
+    expect(actual.body.status).toStrictEqual(expectedBody.status);
+    expect(actual.body.message).toStrictEqual(expectedBody.message);
     expect(actual.statusCode).toBe(200);
   });
 });
+
+
+// import supertest from "supertest";
+// import request from "supertest";
+
+// describe("POST /api/posts", () => {
+//   test("should respond with a 201 status code", async (done) => {
+//     const response = await request(app).post("/api/posts").send({
+//       user_id: 123,
+//       title: "Cucumber",
+//       description: "Nice",
+//       location: "London",
+//       free: false,
+//       price: 10.99,
+//       date: "10/10/2020",
+//     });
+//     expect(response.statusCode).toBe(201);
+//     done();
+//   }, 5000);
+// });
