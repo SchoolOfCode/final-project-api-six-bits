a7689190cc8ed9d7ca5745ac35b14e76cad1a7d4
Sebastian Butoi <butoi.sebastian@yahoo.com>
2022-03-03 10:47:20 +0000

    Added route to get user data

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { insertUser, readUsers } from "../controllers/users.js";
+import { insertUser, readUser, readUsers } from "../controllers/users.js";
 
 const router = express.Router();
 
 router.route("/").get(readUsers).post(insertUser);
+router.route("/:id").get(readUser);
 
 export default router;
