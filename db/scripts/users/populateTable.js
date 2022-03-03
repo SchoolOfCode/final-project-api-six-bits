6a849c8e80210ecc3b5ac4dbdcc9a01f71ec215a
Sebastian Butoi <butoi.sebastian@yahoo.com>
2022-03-03 10:44:03 +0000

    Updated the create user function

--- db/scripts/users/populateTable.js
+++ db/scripts/users/populateTable.js
@@ -1,21 +1,15 @@
 import query from "../../connection.js";
 
 export async function createUser({
+  auth_id,
   first_name,
   last_name,
   phone_number,
-  email,
   user_created,
 }) {
   const result = await query(
-    `INSERT INTO users (first_name, last_name, phone_number, email, user_created) VALUES ($1,$2,$3,$4,$5) RETURNING *;`,
-    [
-      first_name,
-      last_name,
-      phone_number,
-      email,
-      user_created,
-    ]
+    `INSERT INTO users (auth_id, first_name, last_name, phone_number, email, user_created) VALUES ($1,$2,$3,$4,$5,$6) RETURNING *;`,
+    [auth_id, first_name, last_name, phone_number, email, user_created]
   );
   return result.rows;
 }
