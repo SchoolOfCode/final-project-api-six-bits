36fa44b5574e6fdecc18e2e53bf262eebb511e76
Zulfiya Yumagulova <yumagulovazu@gmail.com>
2022-03-04 10:20:12 +0000

    MB, CJ, ZY query getUsersPosts done

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,5 +1,5 @@
 import { createPost } from "../db/scripts/posts/populateTable.js";
-import { getAllPosts } from "../db/scripts/posts/readTable.js";
+import { getAllPosts, getUserPosts } from "../db/scripts/posts/readTable.js";
 
 export async function readPosts(req, res) {
   const data = await getAllPosts();
@@ -15,6 +15,16 @@ export async function insertPost(req, res) {
   res.status(201).json({
     status: "success",
     message: "New post was created",
+    payload: data,
+  });
+}
+
+export async function readUserPosts(req, res) {
+  const data = await getUserPosts(req.params.id);
+
+  res.status(200).json({
+    status: "success",
+    message: "All user's posts",
     payload: data,
   });
 }
