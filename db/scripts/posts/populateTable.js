1a58c1ebfcba4a68b0234becc9fc417b3d44fee4
Sebastian Butoi <butoi.sebastian@yahoo.com>
2022-03-04 14:12:23 +0000

    Insert post query updated

--- db/scripts/posts/populateTable.js
+++ db/scripts/posts/populateTable.js
@@ -1,7 +1,7 @@
 import query from "../../connection.js";
 
 export async function createPost({
-  user_id,
+  auth_id,
   title,
   description,
   location,
@@ -9,17 +9,15 @@ export async function createPost({
   price,
   date,
 }) {
+  const foundUserID = await query(
+    `SELECT users.user_id FROM users LEFT OUTER JOIN posts ON users.user_id = posts.user_id WHERE auth_id = ($1) LIMIT 1;
+  `,
+    [auth_id]
+  );
+  const user_id = foundUserID.rows[0].user_id;
   const result = await query(
     `INSERT INTO posts (user_id, title, description, location, free, price, date) VALUES ($1,$2,$3,$4,$5,$6,$7) RETURNING *;`,
-    [
-      user_id,
-      title,
-      description,
-      location,
-      free,
-      price,
-      date,
-    ]
+    [user_id, title, description, location, free, price, date]
   );
   return result.rows;
 }
