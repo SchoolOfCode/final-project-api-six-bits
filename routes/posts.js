7dab905c816e7e957fb09eeacfceb89127009fef
Sebastian Butoi <butoi.sebastian@yahoo.com>
2022-03-04 15:45:49 +0000

    Added delete route for the post

--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,14 @@
 import express from "express";
-import { insertPost, readPosts, readUserPosts } from "../controllers/posts.js";
+import {
+  insertPost,
+  readPosts,
+  readUserPosts,
+  removePost,
+} from "../controllers/posts.js";
 
 const router = express.Router();
 
-router.route("/").get(readPosts).post(insertPost);
+router.route("/").get(readPosts).post(insertPost).delete(removePost);
 router.route("/:id").get(readUserPosts);
 
 export default router;
