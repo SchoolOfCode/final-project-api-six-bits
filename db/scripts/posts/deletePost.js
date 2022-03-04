66b5c897f45c09a0a688991810884e528bf575fd
Sebastian Butoi <butoi.sebastian@yahoo.com>
2022-03-04 15:49:57 +0000

    Query function refactored

--- db/scripts/posts/deletePost.js
+++ db/scripts/posts/deletePost.js
@@ -13,4 +13,5 @@ export async function deletePost({
     `DELETE FROM posts USING users WHERE posts.user_id = users.user_id AND users.auth_id = ($1) AND posts.title = ($2) AND posts.description = ($3) AND posts.location = ($4) AND posts.free = ($5) AND posts.price = ($6) AND posts.date = ($7);`,
     [auth_id, title, description, location, free, price, date]
   );
+  return result.rows;
 }
