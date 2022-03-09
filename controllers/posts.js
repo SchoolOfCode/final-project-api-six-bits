05c38b6171a45d79acc738ed925c49ffbb309e05
Sebastian Butoi <butoi.sebastian@yahoo.com>
2022-03-09 11:45:59 +0000

    Added express asnycHandler and error message

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,6 +1,7 @@
 import { deletePost } from "../db/scripts/posts/deletePost.js";
 import { createPost } from "../db/scripts/posts/populateTable.js";
 import { getAllPosts, getUserPosts } from "../db/scripts/posts/readTable.js";
+import asyncHandler from "express-async-handler";
 
 export async function readPosts(req, res) {
   const data = await getAllPosts();
@@ -11,14 +12,27 @@ export async function readPosts(req, res) {
   });
 }
 
-export async function insertPost(req, res) {
+export const insertPost = asyncHandler(async (req, res) => {
+  const { auth_id, title, quantity, description, location, price, date } =
+    req.body;
+  if (
+    !auth_id ||
+    !title ||
+    !quantity ||
+    !description ||
+    !location ||
+    !price ||
+    !date
+  ) {
+    throw new Error("Input fields are missing.");
+  }
   const data = await createPost(req.body);
   res.status(201).json({
     status: "success",
     message: "New post was created",
     payload: data,
   });
-}
+});
 
 export async function readUserPosts(req, res) {
   const data = await getUserPosts(req.params.id);
--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,6 @@
 import { createUser } from "../db/scripts/users/populateTable.js";
 import { getAllUsers, getUser } from "../db/scripts/users/readTable.js";
+import asyncHandler from "express-async-handler";
 
 export async function readUsers(req, res) {
   const users = await getAllUsers();
@@ -20,11 +21,24 @@ export async function readUser(req, res) {
   });
 }
 
-export async function insertUser(req, res) {
+export const insertUser = asyncHandler(async (req, res) => {
+  const { auth_id, first_name, last_name, phone_number, email, user_created } =
+    req.body;
+  if (
+    !auth_id ||
+    !first_name ||
+    !last_name ||
+    !phone_number ||
+    !email ||
+    !user_created
+  ) {
+    res.status(400);
+    throw new Error("Fields are missing.");
+  }
   const newUser = await createUser(req.body);
   res.status(201).json({
     status: "success",
     message: "New user was created",
     payload: newUser,
   });
-}
+});
