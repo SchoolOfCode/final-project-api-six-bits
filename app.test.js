aeaea838ae389c324379f0d92c7f22484e846985
Sebastian Butoi <butoi.sebastian@yahoo.com>
2022-03-09 15:56:56 +0000

    Added new test for the user route

--- app.test.js
+++ app.test.js
@@ -17,7 +17,7 @@ describe("GET /api/posts", function () {
 });
 
 describe("POST /api/posts", function () {
-  test("gives us back 201, with a a message and some data", async function () {
+  test("gives us back 201, with a message, and some data", async function () {
     const testingPost = {
       auth_id: "auth0|6218c0cbeae91f006a0ab102",
       title: "Cucumber",
@@ -74,28 +74,57 @@ describe("DELETE /api/posts/:id", function () {
 
 // Users Route
 
-// describe("GET /api/users", function () {
-//   test("gives us back 200, with a message", async function () {
-//     const expectedBody = {
-//       status: "success",
-//       message: "Read all users",
-//     };
-//     const actual = await request(app).get("/api/users");
-//     expect(actual.body.status).toStrictEqual(expectedBody.status);
-//     expect(actual.body.message).toStrictEqual(expectedBody.message);
-//     expect(actual.statusCode).toBe(200);
-//   });
-// });
+describe("GET /api/users", function () {
+  test("gives us back 200, with a message", async function () {
+    const expectedBody = {
+      status: "success",
+      message: "Read all users",
+    };
+    const response = await request(app).get("/api/users");
+    expect(response.body.status).toStrictEqual(expectedBody.status);
+    expect(response.body.message).toStrictEqual(expectedBody.message);
+    expect(response.statusCode).toBe(200);
+  });
+});
 
-// describe("POST /api/users", function () {
-//   test("gives us back 201", async function () {
-//     const response = await request(app).post("/api/users").send({
-//       first_name: "John",
-//       last_name: "Doe",
-//       phone_number: "111111",
-//       email: "john@doe.com",
-//       user_created: "2/3/2022",
-//     });
-//     expect(response.statusCode).toBe(201);
-//   });
-// });
+describe("GET /api/users", function () {
+  test("gives us back 200, wtih a message, and some data", async function () {
+    const expectedBody = {
+      status: "success",
+      message: "Read user",
+      payload: [
+        {
+          user_id: 9,
+          auth_id: "auth0|6218c0cbeae91f006a0ab102",
+          first_name: "mark",
+          last_name: "j",
+          phone_number: "1235",
+          email: "test1@gmail.com",
+          user_created: "7/3/2022",
+        },
+      ],
+    };
+    const auth_id = "auth0|6218c0cbeae91f006a0ab102";
+    const response = await request(app).get(`/api/users/${auth_id}`);
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body.message).toBe(expectedBody.message);
+    expect(response.body.status).toBe(expectedBody.status);
+    expect(response.body.payload[0]).toMatchObject(expectedBody.payload[0]);
+  });
+});
+
+describe("POST /api/users", function () {
+  test("gives us back 201, with a message, and some data", async function () {
+    const newUser = {
+      first_name: "John",
+      last_name: "Doe",
+      phone_number: "111111",
+      email: "john@doe.com",
+      user_created: "2/3/2022",
+    };
+    const response = await request(app).post("/api/users").send(newUser);
+    expect(response.statusCode).toBe(201);
+    expect(response.message).toBe("New user was created");
+  });
+});
